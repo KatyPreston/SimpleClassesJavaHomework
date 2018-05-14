c0da341a9f58824b88c62354c018e2fdcd35dfe5
KatyPreston <prestonkj@hotmail.com>
2018-05-14 16:36:32 +0100

    water bottle tests passing

---
+++ src/test/java/WaterBottleTest.java
@@ -0,0 +1,35 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class WaterBottleTest {
+    WaterBottle waterBottle;
+
+    @Before
+    public void before(){
+        waterBottle = new WaterBottle(100);
+    }
+
+    @Test
+    public void hasVolume(){
+        assertEquals(100, waterBottle.getVolume());
+    }
+
+    @Test
+    public void hasDrink(){
+        assertEquals(90, waterBottle.drink());
+    }
+
+    @Test
+    public void emptiesBottle(){
+        waterBottle.emptyBottle();
+        assertEquals(0, waterBottle.getVolume());
+    }
+
+    @Test
+    public void fillsBottle(){
+        waterBottle.fillBottle();
+        assertEquals(100, waterBottle.getVolume());
+    }
+}
